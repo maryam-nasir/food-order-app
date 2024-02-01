eceff1e1cd467fb2919d0bfbcd67e800d0f4478a
Maryam Nasir <maryamnasir555@yahoo.com>
2024-02-01 15:49:07 +0500

    Add Meal Item component

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,22 @@
+import API_CONSTANTS from '../constants/ApiConstants';
+import { currencyFormatter } from '../utils/formatting';
+
+const MealItem = ({ meal }) => {
+    return (
+        <li className='meal-item'>
+            <article>
+                <img src={`${API_CONSTANTS.BASE_API_URL}${meal.image}`} alt={meal.name} />
+                <div>
+                    <h3>{meal.name}</h3>
+                    <p className='meal-item-price'>{currencyFormatter.format(meal.price)}</p>
+                    <p className='meal-item-description'>{meal.description}</p>
+                </div>
+                <p className='meal-item-actions'>
+                    <button>Add to Cart</button>
+                </p>
+            </article>
+        </li>
+    );
+};
+
+export default MealItem;
