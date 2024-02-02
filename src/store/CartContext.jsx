25e4e9806b146d0eb57bc03d363f4c63a09564ef
Maryam Nasir <maryamnasir555@yahoo.com>
2024-02-02 12:18:36 +0500

    Add cart context to keep track of cart items

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useReducer } from 'react';
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => { },
+    removeItem: (id) => { },
+});
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const existingItemIndex = state.items.findIndex((item) => item.id === action.payload.id);
+        const newItems = [...state.items];
+
+        if (existingItemIndex > -1) {
+            const existingItem = newItems[existingItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1,
+            };
+            newItems[existingItemIndex] = updatedItem;
+        } else {
+            newItems.push({
+                ...action.payload,
+                quantity: 1,
+            });
+        }
+
+        return {
+            ...state,
+            items: newItems,
+        };
+    } else if (action.type === 'REMOVE_ITEM') {
+        const existingItemIndex = state.items.findIndex((item) => item.id === action.payload);
+        const newItems = [...state.items];
+        const existingItem = { ...newItems[existingItemIndex] };
+        if (existingItem.quantity > 1) {
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity - 1,
+            };
+            newItems[existingItemIndex] = existingItem;
+        } else {
+            newItems.splice(existingItemIndex, 1);
+        }
+
+        return {
+            ...state,
+            items: newItems,
+        };
+    }
+
+    return state;
+};
+
+export const CartContextProvider = ({ children }) => {
+    const [cart, dispatch] = useReducer(cartReducer, { items: [] });
+
+    const addItem = (item) => {
+        dispatch({ type: 'ADD_ITEM', payload: item });
+    };
+
+    const removeItem = (id) => {
+        dispatch({ type: 'REMOVE_ITEM', payload: id });
+    };
+
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem,
+    };
+    return (
+        <CartContext.Provider value={cartContext}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartContext;
