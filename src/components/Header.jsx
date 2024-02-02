d5d1736640332aeb2630da45f41e4bcfc8a049a7
Maryam Nasir <maryamnasir555@yahoo.com>
2024-02-02 14:00:31 +0500

    Show total items in cart and open modal on button click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { useContext } from 'react';
 import Button from './UI/Button';
 import { LOGO_IMAGE } from '../constants/AssetConstants';
+import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 const Header = () => {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const totalQuantity = cartCtx.items.reduce((total, item) => total + item.quantity, 0);
+
+    const handleClick = () => {
+        userProgressCtx.showCart();
+    };
+
     return (
         <header id='main-header'>
             <div id='title'>
@@ -9,7 +21,7 @@ const Header = () => {
                 <h1>Yummy Meals</h1>
             </div>
             <nav>
-                <Button>Cart (0)</Button>
+                <Button onClick={handleClick}>Cart ({totalQuantity})</Button>
             </nav>
         </header>
     );
