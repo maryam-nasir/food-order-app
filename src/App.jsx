5b70a34b627965c2bae0733ad3974f2687c6f72d
Maryam Nasir <maryamnasir555@yahoo.com>
2024-02-02 14:02:41 +0500

    Wrap the App component with context providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Header from "./components/Header";
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Meals from './components/Meals';
+import { CartContextProvider } from './store/CartContext';
+import { UserProgressContextProvider } from './store/UserProgressContext';
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
